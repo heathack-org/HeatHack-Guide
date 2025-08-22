c87e79c08e94526154a1c79145a8eca73615182e
Jean Carletta <jeanc939@gmail.com>
2025-08-22 12:21:44 +0100

    Google's full version of the javascript for analytics - has more than the config file supplies

---
+++ _static/analytics.js
@@ -0,0 +1,8 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RHR48JXM45"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RHR48JXM45');
